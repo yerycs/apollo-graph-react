21f2eccede89972a85dafae2d91b109cc3186afc
yerycs <yerycs1015@gmail.com>
2019-11-23 15:39:37 +0800

    write resolver

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -2,11 +2,31 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
     type Query {
-      launches: [Launch]!
-      launch(id: ID!): Launch
-      # Queries for the current user
-      me: User
+        launches( # replace the current launches query with this one.
+            """
+            The number of results to show. Must be >= 1. Default = 20
+            """
+            pageSize: Int
+            """
+            If you add a cursor here, it will only return results _after_ this cursor
+            """
+            after: String
+        ): LaunchConnection!
+        launch(id: ID!): Launch
+        me: User
     }
+
+    """
+    Simple wrapper around our list of launches that contains a cursor to the
+    last item in the list. Pass this cursor to the launches query to fetch results
+    after these.
+    """
+    type LaunchConnection { # add this below the Query type as an additional type.
+        cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
+    }
+    
     type Launch {
       id: ID!
       site: String
@@ -14,6 +34,7 @@ const typeDefs = gql`
       rocket: Rocket
       isBooked: Boolean!
     }
+    
     type Rocket {
       id: ID!
       name: String
@@ -25,16 +46,17 @@ const typeDefs = gql`
       email: String!
       trips: [Launch]!
     }
-    
+
     type Mission {
-      name: String
-      missionPatch(size: PatchSize): String
+        name: String
+        missionPatch(mission: String, size: PatchSize): String
     }
     
     enum PatchSize {
       SMALL
       LARGE
     }
+    
     type Mutation {
       # if false, booking trips failed -- check errors
       bookTrips(launchIds: [ID]!): TripUpdateResponse!
@@ -44,6 +66,7 @@ const typeDefs = gql`
     
       login(email: String): String # login token
     }
+    
     type TripUpdateResponse {
       success: Boolean!
       message: String
